1cfca356025411f5a1efd4d1f590ef9d8b40c7e8
Nimitha J <58389915+Nimitha-jagadeesha@users.noreply.github.com>
2024-09-08 22:03:10 +0530

    Update Navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 export default function Navigation() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style={backgroundColor: #008000}>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style={backgroundColor: "#008000"}>
         <div class="container-fluid">
         <img src={process.env.PUBLIC_URL + "favicon.avif"} height={30} alt="Dream->code->Achieve" />
             <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
